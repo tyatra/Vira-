b4efa20ac4200bba14e372d3dd8148168de3f1ee
tyatra <charles.tze.ming.ng@gmail.com>
2017-01-20 14:31:02 +0800

    Update selfie.js

--- commands/selfie.js
+++ commands/selfie.js
@@ -1,21 +1,21 @@
 request = require('request');
 module.exports = {
   exec: (client, message, content, args) => {
-    message.send("Surely, senpai, please wait a moment");
+    message.send("Really Leader? Please wait...");
     request({
-      url: 'https://safebooru.donmai.us/posts.json?random=true&limit=1&tags=shielder_%28fate%2Fgrand_order%29',
+      url: 'https://safebooru.donmai.us/posts.json?random=true&limit=1&tags=jeanne_%28granblue_fantasy%29',
       json: true
     }, function (err, temp, body) {
       if (!err && body.length > 0 && body[0].file_url) {
         selfie = {
           image: { url: "https://safebooru.donmai.us/" + body[0].file_url },
-          description: "Here you go senpai",
+          description: "Stop! leader I'm not ready!",
           thumbnail: { url: client.emote.embarassed },
           color: 0xff0000
         }
         message.channel.sendMessage("", { embed: selfie });
       } else {
-        message.send("I'm sorry senpai, the camera is broken");
+        message.send("leader, the camera is broken");
         console.log(err);
         console.log(body);
       }
